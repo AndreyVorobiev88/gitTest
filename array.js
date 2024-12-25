d7913ae243163c9e7a57249cb32cb6533cda7f3f
AndreyVorobiev88 <Foroven@mail.ru>
2024-12-25 14:50:51 +0300

    modified:   array.js

--- array.js
+++ array.js
@@ -7,25 +7,16 @@
 
 var singleNumber = function(nums) {
 
-    let rez;
-    
-    if (nums.length == 1) return nums[0];
-    else
-    {
-        nums.sort();
-        let i = 0;
-        while (i < nums.length) 
-        {
-            if (nums[i] == nums[i+1]) {
-                i+=2;
-            }
-            else
-            {
-                rez = nums[i];
-                break;
-            }
+    nums.sort();
+    let i = 0;
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] == nums[i+1]) {
+            i++;
         }
+        else return nums[i];
+
     }
 
-    return rez;
+    return underfined;
 };
