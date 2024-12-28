49d52d48c5188dc0b5a07c979932f0e28643009e
AndreyVorobiev88 <Foroven@mail.ru>
2024-12-28 16:15:49 +0300

    modified:   ValidAnagram.js

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -3,6 +3,44 @@
  * @param {string} t
  * @return {boolean}
  */
+
+function strToObj(str) {
+
+    var obj = new Object();
+    var arr = str.split('');
+
+    arr.forEach(element => {
+        if (obj.hasOwnProperty(element))
+            obj[element] += 1
+        else
+            obj[element] = 1
+    });
+
+    return obj
+}
+
+function isDeepEqual(obj1, obj2) {
+    var keys1 = Object.keys(obj1);
+    var keys2 = Object.keys(obj2);
+    var Flag = true;
+ 
+    if (keys1.length != keys2.length) 
+        return false;
+    
+    keys1.forEach(element => {
+        if (obj1[element] != obj2[element])  
+            Flag = false; 
+    });
+ 
+    return Flag;
+}
+
+
 var isAnagram = function(s, t) {
+
+    var objS = strToObj(s);
+    var objT = strToObj(t);
+
+    return (isDeepEqual(objS, objT));
     
 };
