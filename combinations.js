b72d047da63b2009adab81e1bd23ed9d9c36e078
AndreyVorobiev88 <Foroven@mail.ru>
2025-01-15 16:21:00 +0300

    modified:   combinations.js

--- combinations.js
+++ combinations.js
@@ -2,22 +2,40 @@
  * @param {string} digits
  * @return {string[]}
  */
+
+function f(combinations, letters) {
+
+    let rez = [];
+
+    rez = combinations.flatMap(comb => {
+
+        return letters.map(letter => {
+
+           return comb + letter
+
+        })
+
+    })
+
+
+    return rez;
+
+}
+
 var letterCombinations = function(digits) {
     
     const letters = [, , ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']];
     const nums = digits.split("");
 
-    const rez = [];
-    const str;
+    let rez = [];
 
-    nums.forEach(digit => {
+    if (!digits.length) return rez
 
-        rez = letters[+digit].map(letter =>{
+    nums.forEach((digit, i) => {
 
-            return str + letter
-
-        })
-            
+      if (!i)  rez = letters[+digit];
+      else 
+        rez = f(rez, letters[+digit]);  
         
     });
 
