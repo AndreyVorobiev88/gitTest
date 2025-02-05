49bd569ceb4595ba62a7a6b66c78bd80d1c4febc
AndreyVorobiev88 <Foroven@mail.ru>
2025-02-05 16:50:07 +0300

    new file:   FrontendApplicationBuildTools 	modified:   ValidAnagram.js

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -22,17 +22,16 @@ function strToObj(str) {
 function isDeepEqual(obj1, obj2) {
     var keys1 = Object.keys(obj1);
     var keys2 = Object.keys(obj2);
-    var Flag = true;
  
     if (keys1.length != keys2.length) 
         return false;
     
     keys1.forEach(element => {
         if (obj1[element] != obj2[element])  
-            Flag = false; 
+            return false; 
     });
  
-    return Flag;
+    return true;
 }
 
 
